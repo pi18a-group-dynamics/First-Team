908a70ea35097ea5262090452499951278f30b7d
Kirill Danilchuk <kirill.danilchuk01@mail.ru>
2020-05-13 15:55:50 +0300

    Code review completed

--- ingredients.cpp
+++ ingredients.cpp
@@ -7,14 +7,15 @@
 #include <QMessageBox>
 #include <QDebug>
 #include <QSqlError>
-Ingredients::Ingredients(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::Ingredients)
-{
-    ui->setupUi(this);
+
+Ingredients::Ingredients(QWidget *parent)
+: QDialog(parent)
+, ui_(new Ui::Ingredients) {
+    ui_->setupUi(this);
     connect_database();
     form_init();
 }
+
 void Ingredients::connect_database() {
     database_ = QSqlDatabase::addDatabase("QPSQL");
     database_.setPort(5432);
@@ -27,95 +28,88 @@ void Ingredients::connect_database() {
         std::terminate();
     }
 }
+
 void Ingredients::form_init() {
     setWindowTitle("Ингредиенты");
-    model = new QSqlQueryModel;
-    model->setQuery("SELECT id,name AS Название, meansurement AS Измерение FROM ingredients;");
-    ui->ingr_table_->setModel(model);
-    ui->name_ingr_line_->setValidator(new QRegExpValidator(QRegExp("[А-Я]{1}[a-я]{1,}")));
-    ui->mer_ingr_line_->setValidator(new QRegExpValidator(QRegExp("[А-Я]{1}[a-я]{1,}")));
-    ui->ingr_table_->setColumnHidden(0, true);
-    QObject::connect(ui->ingr_table_,&QTableView::clicked,[this](const QModelIndex & index){
-        ui->name_ingr_line_->setText(model->data(model->index(index.row(),1)).toString());
-        ui->mer_ingr_line_->setText(model->data(model->index(index.row(),2)).toString());
-        current_id= model->data(model->index(index.row(),0)).toString();
+    model_ = new QSqlQueryModel;
+    model_->setQuery("SELECT id, name AS Название, meansurement AS Измерение FROM ingredients ORDER BY name;");
+    ui_->ingr_table_->setModel(model_);
+    ui_->name_ingr_line_->setValidator(new QRegExpValidator(QRegExp("[А-Я]{1}[a-я]{1,}")));
+    ui_->mer_ingr_line_->setValidator(new QRegExpValidator(QRegExp("[А-Я]{1}[a-я]{1,}")));
+    ui_->ingr_table_->setColumnHidden(0, true);
+    ui_->ingr_table_->horizontalHeader()->setSectionResizeMode(1, QHeaderView::Stretch);
+    ui_->ingr_table_->horizontalHeader()->setSectionResizeMode(2, QHeaderView::Stretch);
+    ui_->ingr_table_->setSelectionMode(QTableView::SingleSelection);
+    QObject::connect(ui_->ingr_table_, &QTableView::clicked, [this](const QModelIndex& index) {
+        ui_->name_ingr_line_->setText(model_->data(model_->index(index.row(), 1)).toString());
+        ui_->mer_ingr_line_->setText(model_->data(model_->index(index.row(), 2)).toString());
     });
 }
 
-void Ingredients::update_table(){
-    model->setQuery(model->query().lastQuery());
-    ui->name_ingr_line_->setText("");
-    ui->mer_ingr_line_->setText("");
+void Ingredients::update_table() {
+    model_->setQuery(model_->query().lastQuery());
+    ui_->name_ingr_line_->setText("");
+    ui_->mer_ingr_line_->setText("");
 }
 
-Ingredients::~Ingredients()
-{
-    delete ui;
+Ingredients::~Ingredients() {
+    delete ui_;
 }
 
-void Ingredients::on_add_ingr_btn__clicked()
-{
-    if(ui->mer_ingr_line_->text().isEmpty() || ui->name_ingr_line_->text().isEmpty()){
-        QMessageBox::warning(nullptr,"Нет введеных данных", "Введите данные");
-    } else {
+void Ingredients::on_add_ingr_btn__clicked() {
+    if (ui_->mer_ingr_line_->text().isEmpty() || ui_->name_ingr_line_->text().isEmpty()) {
+        QMessageBox::warning(nullptr, "Нет введеных данных", "Введите данные");
+        return;
+    }
     QSqlQuery query;
     query.prepare("INSERT INTO ingredients "
                   "VALUES(default,"
                   ":name,"
                   ":meansurement);");
-    query.bindValue(":name",ui->name_ingr_line_->text());
-    query.bindValue(":meansurement",ui->mer_ingr_line_->text());
+    query.bindValue(":name", ui_->name_ingr_line_->text());
+    query.bindValue(":meansurement", ui_->mer_ingr_line_->text());
     if (!query.exec()) {
-            qDebug() << "error insert into " << "ingredients";
-            qDebug() << query.lastError().text();
-            qDebug() << query.lastQuery();
-        }
+        qDebug() << "error insert into ingredients";
+        qDebug() << query.lastError().text();
+        qDebug() << query.lastQuery();
+    }
     update_table();
-    }
 }
 
-void Ingredients::on_erase_ingr_btn__clicked()
-{
-    if(ui->mer_ingr_line_->text().isEmpty() || ui->name_ingr_line_->text().isEmpty()){
-        QMessageBox::warning(nullptr,"Не выбран ингредиент", "Выберете ингредиент для удаления");
-    } else {
-
-        QSqlQuery query;
-        if(query.exec("SELECT id FROM ingredients WHERE id="+current_id+";")&&  query.last()==true){
-
-        if(query.exec("DELETE FROM ingredients WHERE id="+current_id+";")){
-            update_table();
-             QMessageBox::information(nullptr,"Удаление", "Удален ингредиент");
-        } else {
-             QMessageBox::information(nullptr,"Удаление", "Не удалось удалить");
-        }
-    } else {
-           QMessageBox::information(nullptr,"Удаление", "Запись не обнаружена");
-        }
-
+void Ingredients::on_erase_ingr_btn__clicked() {
+    const QAbstractItemModel* model = ui_->ingr_table_->model();
+    QString current_id = model->data(model->index(ui_->ingr_table_->currentIndex().row(), 0)).toString();
+    if (ui_->mer_ingr_line_->text().isEmpty() || ui_->name_ingr_line_->text().isEmpty()) {
+        QMessageBox::warning(nullptr, "Не выбран ингредиент", "Выберете ингредиент для удаления");
+        return;
     }
+    if (!QSqlQuery().exec("DELETE FROM ingredients WHERE id = " + current_id + ';')) {
+        QMessageBox::warning(nullptr, "Не удалось удалить", "Запись не обнаружена");
+        return;
+    }
+    update_table();
+    QMessageBox::information(nullptr, "Удаление", "Удален ингредиент");
 }
 
-void Ingredients::on_change_ingr_btn__clicked()
-{
-    if(ui->mer_ingr_line_->text().isEmpty() || ui->name_ingr_line_->text().isEmpty()){
-        QMessageBox::warning(nullptr,"Не выбран ингредиент", "Выберете ингредиент для изменения");
-    } else {
-
-        QSqlQuery query;
-        if(query.exec("SELECT id FROM ingredients WHERE id="+current_id+";")&&  query.last()==true){
-
-        if(query.exec("UPDATE ingredients SET name= '"+ui->name_ingr_line_->text()+
-                      "', meansurement = '"+ui->mer_ingr_line_->text()+"'"
-                      """WHERE id="+current_id+";")){
-            update_table();
-             QMessageBox::information(nullptr,"Изменение", "Изменен ингредиент");
-        } else {
-             QMessageBox::information(nullptr,"Изменениее", "Не удалось изменить");
-        }
-    } else {
-           QMessageBox::information(nullptr,"Изменение", "Запись не обнаружена");
-        }
-
+void Ingredients::on_change_ingr_btn__clicked() {
+    const QAbstractItemModel* model = ui_->ingr_table_->model();
+    QString current_id = model->data(model->index(ui_->ingr_table_->currentIndex().row(), 0)).toString();
+    if (ui_->mer_ingr_line_->text().isEmpty() || ui_->name_ingr_line_->text().isEmpty()) {
+        QMessageBox::warning(nullptr, "Не выбран ингредиент", "Выберете ингредиент для изменения");
+        return;
     }
+    QSqlQuery query;
+    query.prepare("UPDATE ingredients SET "
+                  "name = :name, "
+                  "meansurement = :meansurement "
+                  "WHERE id = :id;");
+    query.bindValue(":name", ui_->name_ingr_line_->text());
+    query.bindValue(":meansurement", ui_->mer_ingr_line_->text());
+    query.bindValue(":id", current_id);
+    if (!query.exec()) {
+        QMessageBox::warning(nullptr, "Не удалось изменить", "Запись не обнаружена");
+        return;
+    }
+    update_table();
+    QMessageBox::information(nullptr, "Изменение", "Ингредиент изменён");
 }
--- ingredients.h
+++ ingredients.h
@@ -9,29 +9,22 @@ namespace Ui {
 class Ingredients;
 }
 
-class Ingredients : public QDialog
-{
+class Ingredients : public QDialog {
     Q_OBJECT
-
 public:
     explicit Ingredients(QWidget *parent = nullptr);
-     void connect_database();
-     void form_init();
-     void update_table();
+    void connect_database();
+    void form_init();
+    void update_table();
     ~Ingredients();
-
 private slots:
-     void on_add_ingr_btn__clicked();
-
-     void on_erase_ingr_btn__clicked();
-
-     void on_change_ingr_btn__clicked();
-
+    void on_add_ingr_btn__clicked();
+    void on_erase_ingr_btn__clicked();
+    void on_change_ingr_btn__clicked();
 private:
-    Ui::Ingredients *ui;
+    Ui::Ingredients* ui_;
     QSqlDatabase database_;
-    QSqlQueryModel * model;
-     QString current_id;
+    QSqlQueryModel* model_;
 };
 
 #endif // INGREDIENTS_H
